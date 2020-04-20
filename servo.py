defc1759b758cacecacec275c2e4b3e814755116
Agustin Velasco Torres <aguslb@icloud.com>
2020-04-20 00:13:00 -0500

    remove folder

---
+++ servo.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+import time
+
+#def printSleep (howLong) :
+#  arrow = '-->'
+#  once = False
+#  while howLong > 0 :
+#    if once == False and howLong <= 10 :
+#      arrow = '--->'
+#      once = True
+#    time.sleep(1)
+#    howLong -= 1
+#    print (arrow + str(howLong), end='\r')
+#Default Settings
+initPosition = 12
+finalPosition = 9
+slShort = 1
+slLong = 100
+gpiOutput = 11
+gpiLEDOut = 33
+workingTimes = 0
+#Setting GPIO and servo config
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(gpiOutput, GPIO.OUT)
+p = GPIO.PWM(gpiOutput, 50)
+p.start(initPosition)
+#Setting GPIO led
+GPIO.setup(gpiLEDOut,GPIO.OUT)
+#Runtine
+try:
+    while True:
+        p.ChangeDutyCycle(initPosition)
+        GPIO.output(13,True)
+        time.sleep(slLong)
+        p.ChangeDutyCycle(finalPosition)
+        GPIO.output(11,False)
+        time.sleep(slShort)
+        workingTimes += 1
+        print (workingTimes + '<-- times runned :)', end='\r')
+except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
