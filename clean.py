9f77e350af8fa36de09e0b5c8fea75beac338bf9
Agustin Velasco Torres <aguslb@icloud.com>
2020-04-20 01:21:00 -0500

    test

--- clean.py
+++ clean.py
@@ -1,6 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 
+gpiOutput = 11
+gpiLEDWork = 13
+gpiLedIdle = 15
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(gpiOutput, GPIO.OUT)
+GPIO.setup(gpiLEDWork, GPIO.OUT)
+GPIO.setup(gpiLedIdle, GPIO.OUT)
 GPIO.cleanup()
-
